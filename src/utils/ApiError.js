c3570144aeca4ad7f4074f8ea708bdc03eb4091c
LokeshPurohit <purohitlokesh46@gmail.com>
2024-08-02 18:43:16 +0530

    Creating the AsyncHandler, ApiResponse and the ApiError utilities

---
+++ src/utils/ApiError.js
@@ -0,0 +1,23 @@
+class ApiError extends Error {
+  constructure(
+    statusCode,
+    message = "Something went wrong",
+    stack = "",
+    Errors = []
+  ) {
+    super(message);
+    this.statusCode = statusCode;
+    this.message = message;
+    this.Errors = Errors;
+    this.data = null;
+    this.success = false;
+
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
+
+export { ApiError };
